2206b8cd30c6fe9a9f04a426bf6e83252db099a8
JugglerX <rob@jugglerdigital.com>
2016-06-17 15:27:16 -0700

    homepage prototype views for profile create, skillsets and skill

--- src/tshape/static/js/scripts.js
+++ src/tshape/static/js/scripts.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
         centerMode: true,
         centerPadding: '60px',
         slidesToShow: 3,
-        dots: true,
+        dots: true
     });
+    $('body.page-profile-skillsets').on('click', '#add-skillset-javascript', function(e) {
+        e.preventDefault();
+        $('.skillsets--added li').remove();
+        $('.skillsets--added').prepend("<li><a href='/profile/skillsets/javascript/' class='button secondary'>Javascript</a></li>")
+    })
 });
